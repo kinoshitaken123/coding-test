2b7ecfa8d5d3f7269bc57553c5ba454769c34b92
“kinoshitaken123” <k1k7s9e3@gmail.com>
2021-10-19 20:22:17 +0900

    [AddBinary]

---
+++ etc/nginx/conf.d/AddBinary2.rb
@@ -0,0 +1,10 @@
+# 2つの二進数文字列aとbが与えられた場合、
+# それらの合計を二進数文字列として返します。
+# Input: a = "1010", b = "1011"
+# Output: "10101"
+
+def add_binary(a, b)
+    return (a.to_i(2) + b.to_i(2)).to_s(2)
+  end
+  
+  puts add_binary("1010","1011")
